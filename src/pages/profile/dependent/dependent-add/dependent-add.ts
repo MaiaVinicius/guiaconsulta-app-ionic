156da73f2c9ac1423e4b4253b7e4f67be96d69f9
MaiaVinicius <viniciusmaia.tx@gmail.com>
2017-07-29 22:57:51 -0300

    adde example dependent

--- src/pages/profile/dependent/dependent-add/dependent-add.ts
+++ src/pages/profile/dependent/dependent-add/dependent-add.ts
@@ -2,23 +2,23 @@ import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
 /**
- * Generated class for the DependentPage page.
+ * Generated class for the DependentAddPage page.
  *
  * See http://ionicframework.com/docs/components/#navigation for more info
  * on Ionic pages and navigation.
  */
 
 @Component({
-  selector: 'page-dependent',
-  templateUrl: 'dependent.html',
+  selector: 'page-dependent-add',
+  templateUrl: 'src/pages/profile/dependent/dependent-add/dependent-add.html',
 })
-export class DependentPage {
+export class DependentAddPage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad DependentPage');
+    console.log('ionViewDidLoad DependentAddPage');
   }
 
 }
