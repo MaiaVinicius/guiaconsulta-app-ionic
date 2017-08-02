391a1ef83dc390fb7d756a6c636ed15fd97bd77e
MaiaVinicius <viniciusmaia.tx@gmail.com>
2017-08-01 23:38:38 -0300

    created doctor profile page

---
+++ src/pages/doctor-profile/doctor-profile.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the DoctorProfilePage page.
+ *
+ * See http://ionicframework.com/docs/components/#navigation for more info
+ * on Ionic pages and navigation.
+ */
+
+@Component({
+  selector: 'page-doctor-profile',
+  templateUrl: 'doctor-profile.html',
+})
+export class DoctorProfilePage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad DoctorProfilePage');
+  }
+
+}
